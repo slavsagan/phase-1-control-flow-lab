cc63e31cc28712bc49174f8f0db64eb448b04b60
Slav <itiksolution@gmail.com>
2022-09-20 21:01:41 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+  let result
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (distance > 2500) {
+    return (result = 'No can do.')
+  } else if (distance > 2000) {
+    return (result = 'I will gladly take your thirty bucks.')
+  } else if (distance > 400 && distance <= 1500) {
+    return 'That will be twenty bucks.'
+  } else if (distance <= 400) {
+    return (result = 'This one is on me!')
+  }
 }
+console.log(scuberGreetingForFeet(199))
+console.log(scuberGreetingForFeet(1500))
+console.log(scuberGreetingForFeet(2001))
+console.log(scuberGreetingForFeet(2501))
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
+console.log(ternaryCheckCity('NYC'))
+
+function switchOnCharmFromTip(trip) {
+  switch (trip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}
+console.log(switchOnCharmFromTip('generous'))
+console.log(switchOnCharmFromTip('not as generous'))
+console.log(switchOnCharmFromTip('thanks for everything'))
